04400832f496e976ff344d22cf2b5b7640615660
kelsiec <kelsiec@maizeanalytics.com>
2019-02-12 12:05:41 -0800

    recursive solution

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,50 +1,37 @@
 import board.Board;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Stack;
 
 public class SudokuSolver {
     private Board board;
-    private Board solution;
 
     private SudokuSolver(String filePath) throws IOException {
         this.board = new Board(filePath);
-        this.solution = new Board();
     }
 
-
-    private void solve() {
-        HashSet<String> visited = new HashSet<>();
-
-        Stack<Board> stack = new Stack<>();
-        stack.add(this.board);
-
-        while (!stack.isEmpty()) {
-            Board node = stack.pop();
-            if (node.isSolved()) {
-                this.solution = node;
-                stack.clear();
-            } else {
-                visited.add(node.toString());
-                ArrayList<Board> neighbors = node.getNeighbors();
-
-                for (Board neighbor : neighbors) {
-                    if (!visited.contains(neighbor.toString())) {
-                        stack.push(neighbor);
-                    }
+    private Board solve(Board board) {
+        if (board.isSolved()) {
+            return board;
+        } else {
+            for (Board neighbor : board.getNeighbors()) {
+                Board potentialSolution = solve(neighbor);
+                if (potentialSolution != null) {
+                    return potentialSolution;
                 }
             }
         }
+
+        return null;
+    }
+
+
+    private Board solve() {
+        return solve(this.board);
     }
 
     public static void main(String [] args) throws IOException {
         SudokuSolver solver = new SudokuSolver("src/test/resources/trivial_puzzle.sdk");
         System.out.println(solver.board);
-
-        solver.solve();
-
-        System.out.println(solver.solution);
+        System.out.println(solver.solve());
     }
 }
