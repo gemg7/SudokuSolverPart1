62ba7c68fd06b646a57913fc406a832c18aaa753
gemg7 <Lightred123>
2019-02-20 06:02:21 -0800

    split into classes, couldn't figure out how.

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -3,36 +3,12 @@ import board.Board;
 
 import java.io.IOException;
 
-public class SudokuSolver {
-    private Board board;
+public abstract class SudokuSolver {
+    public Board board;
+   // public SudokuSolver() {}
+//    public SudokuSolver(String filePath) throws IOException {
+//        this.board = new Board(filePath);
+//    }
+    public abstract Board solve();
 
-    private SudokuSolver(String filePath) throws IOException {
-        this.board = new Board(filePath);
-    }
-
-    private Board solve(Board board) {
-        if (board.isSolved()) {
-            return board;
-        } else {
-            for (Board neighbor : board.getNeighbors()) {
-                Board potentialSolution = solve(neighbor);
-                if (potentialSolution != null) {
-                    return potentialSolution;
-                }
-            }
-        }
-
-        return null;
-    }
-
-
-    private Board solve() {
-        return solve(this.board);
-    }
-
-    public static void main(String [] args) throws IOException {
-        SudokuSolver solver = new SudokuSolver("src/test/resources/trivial_puzzle.sdk");
-        System.out.println(solver.board);
-        System.out.println(solver.solve());
-    }
 }
