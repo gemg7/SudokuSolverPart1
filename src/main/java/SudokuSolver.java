801d776b59f5519f48a601c7353d49bb50d5ef51
gemg7 <Lightred123>
2019-02-21 22:29:27 -0800

    finished

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,14 +1,59 @@
-// Eric Anthony
 import board.Board;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public abstract class SudokuSolver {
     public Board board;
-   // public SudokuSolver() {}
-//    public SudokuSolver(String filePath) throws IOException {
-//        this.board = new Board(filePath);
-//    }
+    public SudokuSolver(String filePath) throws IOException {
+        this.board = new Board(filePath);
+    }
     public abstract Board solve();
+}
 
+class RecursiveSolver extends SudokuSolver {
+    private Board solve(Board board) {
+        if (board.isSolved()) {
+            return board;
+        } else {
+            for (Board neighbor : board.getNeighbors()) {
+                Board potentialSolution = solve(neighbor);
+                if (potentialSolution != null) {
+                    return potentialSolution;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public Board solve() {
+        return solve(this.board);
+    }
+
+    public RecursiveSolver(String filePath) throws IOException {
+        super(filePath);
+    }
+}
+
+class DFSSolver extends SudokuSolver {
+    public Board solve() {
+        Stack<Board> stack = new Stack<>();
+        stack.push(board);
+
+        while(!stack.empty()) {
+            Board element = stack.pop();
+            if (element.isSolved()) { return element;}
+            ArrayList<Board> neigh = element.getNeighbors();
+            for(Board b : neigh) {
+                stack.push(b);
+            }
+        }
+        return null;
+    }
+
+    public DFSSolver(String filePath) throws IOException {
+        super(filePath);
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class main {
     public static void main(String [] args) throws IOException {
-        RecursiveSolver solver = new SudokuSolver("src/test/resources/trivial_puzzle.sdk");
+        SudokuSolver solver = new DFSSolver("src/test/resources/trivial_puzzle.sdk");
         System.out.println(solver.board);
         System.out.println(solver.solve());
     }
